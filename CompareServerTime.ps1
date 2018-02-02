642b774353e338d0f275ea4514c4f71b213f0ed2
Stefano Gioia <stgioia@gmail.com>
2018-02-02 13:41:59 +0100

    Adding scripts.
    Self-explanatory title.

---
+++ CompareServerTime.ps1
@@ -0,0 +1,36 @@
+<#
+Pretty simple script to compare the time of all local Windows servers against a single server (i.e. Domain Controller)
+#>
+
+Import-Module ActiveDirectory
+
+$ErrorActionPreference = "SilentlyContinue"
+
+# Threshold for reporting in seconds
+$DiffThreshold = 300
+
+$ErrorMsg = ""
+
+$Master = "dc1.domain.com"
+
+$OutputTXT = "E:\PRTG\CompareServerTime-Result.txt"
+
+# Getting all Windows Servers in the current domain
+$ServerList = Get-ADComputer -Filter {OperatingSystem -Like "Windows *Server*"}  | Select -expandproperty dnshostname
+
+foreach ($row in $ServerList)
+{
+ $time1 = ([WMI]'').ConvertToDateTime((gwmi win32_operatingsystem -computername $row).LocalDateTime)
+ $time2 = ([WMI]'').ConvertToDateTime((gwmi win32_operatingsystem -computername $Master).LocalDateTime) 
+
+ $timediff = New-TimeSpan $Time1 $Time2
+
+ If ([System.Math]::Abs($timediff.TotalSeconds) -gt $DiffThreshold) 	{
+	$ErrorMsg = $ErrorMsg + $row + " " + $timediff.TotalSeconds + " seconds" + " | "
+					}
+}
+
+
+If ($ErrorMsg) {
+$ErrorMsg | Out-File $OutputTXT
+}
